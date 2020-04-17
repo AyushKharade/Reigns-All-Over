8c6eef7fe34993cfdb24274cebbd9768835d3ae4
AyushKharade <ayush.kharade999@gmail.com>
2020-04-16 21:03:18 -0400

    setting up combat layers

---
+++ Reigns All Over/Reigns All Over/Assets/Scripts/Player/Combat.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class deals with combat system and animation
+/// </summary>
+public class Combat : MonoBehaviour
+{
+    public bool inCombat;
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (inCombat)
+            {
+                inCombat = false;
+                animator.SetBool("inCombat",false);
+            }
+            else
+            {
+                inCombat = true;
+                animator.SetBool("inCombat", true);
+            }
+        }
+    }
+}
