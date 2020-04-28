708c579a234d779b6dcb39d78e4f9987d8cd1751
AyushKharade <ayush.kharade999@gmail.com>
2020-04-28 18:25:43 -0400

    setup npc & navmesh

---
+++ Reigns All Over/Reigns All Over/Assets/Scripts/NPC & Enemy/NPC_Attributes.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/// <summary>
+/// Stores data & general functions.
+/// </summary>
+public class NPC_Attributes : MonoBehaviour
+{
+    [Header("Details")]
+    public string npcName;
+    [TextArea(3, 3)]
+    public string desc;
+
+    [Header("Variables")]
+    public int health;
+
+    public enum NPC_Type { Villager, Guard, Creature, Bandit};
+    public NPC_Type npcType = new NPC_Type();
+
+    public enum combat_Type { Flee, Melee, Magic, Creature};
+    public combat_Type combatType = new combat_Type();
+
+    [Header("Navmesh Test")]
+    public Transform destination;
+    bool destinationSet;
+    public float velocity;
+    private NavMeshAgent navmeshRef;
+
+    private void Start()
+    {
+        navmeshRef = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (!destinationSet)
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                destinationSet = true;
+                GoToDestination();
+            }
+        }
+        velocity = navmeshRef.velocity.magnitude;
+    }
+
+    void GoToDestination()
+    {
+        navmeshRef.SetDestination(destination.position);
+    }
+}
